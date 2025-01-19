831ac8b955ba6a52df70e59a014fff8e11d958d0
reid <reid@scogg.in>
2025-01-18 18:53:47 -0600

    fix keygen

--- cli/cli.go
+++ cli/cli.go
@@ -538,12 +538,17 @@ var GetKeyfileCmd = &cobra.Command{
 				return fmt.Errorf("invalid life value: %v", err)
 			}
 		}
+		lifeInt -= 1
 		wallet := keygen.GenerateWallet(masterTicket, uint32(pointInt), "", uint(lifeInt), true)
 		pointKey := strings.TrimPrefix(pInfo.Network.Keys.Crypt, "0x")
-		if wallet.Network.Keys.Crypt.Public != pointKey && life != "" {
+		if wallet.Network.Keys.Crypt.Public != pointKey {
 			return fmt.Errorf("could not generate public key matching PKI; 0x%s / %s", wallet.Network.Keys.Crypt.Public, pInfo.Network.Keys.Crypt)
 		}
+		lifeInt += 1
 		keyfile, err := roller.Keyfile(wallet.Network.Keys.Crypt.Private, wallet.Network.Keys.Auth.Private, patp, lifeInt)
+		if err != nil {
+			return fmt.Errorf("error generating keyfile: %v", err)
+		}
 		fmt.Println(keyfile)
 		var outDir string
 		if output != "" {
--- roller/roller.go
+++ roller/roller.go
@@ -147,7 +147,6 @@ func ValidatePrivateKey(privateKey *ecdsa.PrivateKey) error {
 }
 
 func keyNoun(point *big.Int, revision int, bnsec *big.Int) noun.Cell {
-	revision -= 1
 	return noun.MakeNoun([]interface{}{
 		[]interface{}{1, 0},            // [1 0]
 		point,                          // point/ship number
